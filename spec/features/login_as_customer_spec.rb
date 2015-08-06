25e2b62491cecf449014c65fb32ced3b1665ad2f
Rick Bacci <rickbacci@gmail.com>
2015-08-05 23:46:29 -0600

    add login as customer spec
    - add link to home page

---
+++ spec/features/login_as_customer_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "User", type: :feature do
+  include OmniAuthUser
+
+  before do
+    OmniAuth.config.mock_auth[:google_oauth2] = nil
+    Capybara.app = Foobr::Application
+    stub_omniauth
+  end
+
+
+  it 'can login as a customer' do
+    VCR.use_cassette("login_as_customer") do
+      visit root_path
+
+      assert_equal 200, page.status_code
+
+      click_link "Sign up as a Customer"
+
+      expect(page).to have_css("#sign_out")
+      expect(page).to have_link("Sign out")
+      expect(page).to have_link("Profile")
+    end
+  end
+end
+
