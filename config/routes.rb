772605357fef7028c001a07075700956fa57ad73
Rick Bacci <rickbacci@gmail.com>
2015-08-05 22:48:28 -0600

    fix route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
 
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
   resources :sessions, only: [:create, :destroy]
   resources :home, only: [:show]
-  resources :user, only: [:show]
+  resources :users, only: [:show]
 
   root to: "home#show"
 end
