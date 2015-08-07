008c668ca421b3222d51973ccc751a1f87167ab9
Rick Bacci <rickbacci@gmail.com>
2015-08-07 15:34:58 -0600

    add jquery for searhing by name

--- app/assets/javascripts/specialties.js
+++ app/assets/javascripts/specialties.js
@@ -1,2 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  // Get a list of all developers
+  var allDevelopers = $('tr.developer');
+
+  // Add a 'change' event handler for the input element
+  $('input.search-specialty').on('change keyup', function(e) {
+    //  Get the value of the search text
+    var searchText = e.target.value;
+
+    $(allDevelopers).each(function (index, developer) {
+      var name = $(developer).find('.name').text().toLowerCase();
+
+      var matches = name.toLowerCase().indexOf(searchText) !== -1;
+
+      $(developer).toggle(matches);
+    });
+  });
+});
+
