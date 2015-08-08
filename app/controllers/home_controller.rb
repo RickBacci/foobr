2213e484f7e307c8fc26b3719ccca5e39e6d0dca
Rick Bacci <rickbacci@gmail.com>
2015-08-08 17:25:14 -0600

    Guest users should still see developers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def show
-    #@user = User.find(session[:user_id]) if current_user
     @user = User.find(session[:user_id]) if current_user
-    @developers = User.where(role: 'developer') if current_user
+    @developers = User.where(role: 'developer')
   end
 end
