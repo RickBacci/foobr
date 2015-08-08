df07a2c77dad9c3b8ae1483aacda95e6884bf99a
Rick Bacci <rickbacci@gmail.com>
2015-08-08 17:24:49 -0600

    Add seed data for developers and specialties

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,32 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+names = %w(rick sally justin drew max viki will jorge jeff steve josh josh dj mike rachel vane)
+specialties = %w(ruby rails php python c c++ angular javascript html materialize bootstrap ios android)
+
+
+20.times do |val|
+  first_name = names.sample
+  last_name = names.sample
+  full_name = "#{first_name} #{last_name}"
+  user_specialties = specialties.sample(rand(1..6))
+
+  User.create!(
+    {
+      "provider"         => "google_oauth2",
+      "uid"              => "108697336751226714658".chars.shuffle,
+      "name"             => full_name,
+      "email"            => "brownieelf175@gmail.com",
+      "oauth_token"      => "lasdflasfdd232342342dfsddf".chars.shuffle,
+      "oauth_expires_at" => 1438814378,
+      "first_name"       => first_name,
+      "last_name"        => last_name,
+      "image"            => "http://placehold.it/100x100",
+      "role"             => 'developer'
+    })
+
+  user_specialties.each { |s| User.last.specialties.create(specialty_name: s) }
+end
+
