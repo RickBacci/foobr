9b874ebb55bd252e0e038c9ff200148527505b56
Rick Bacci <rickbacci@gmail.com>
2015-08-10 21:23:26 -0600

    remove http://localhost:3000 from link
    - move specialties to header of collection

--- app/assets/javascripts/get_specialties.js
+++ app/assets/javascripts/get_specialties.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
 function getSpecialties() {
   $.ajax({
-    url: 'http://localhost:3000/get_specialties',
+    url: '/get_specialties',
     type: 'GET',
     success: function(tags) {
       $.each(tags, function(index, tag){
@@ -41,6 +41,7 @@ function postSpecialties() {
     data: {specialty: {specialty_name: specialtyName }},
     dataType: "json",
     success: function(refreshSpecialties) {
+
       $('ul.collection').prepend(
           "<li class='collection-item'"
           + "<button id='delete-specialty' name='delete-button' class=''>Delete</button>"
