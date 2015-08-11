7ebe6e51f31762beb74d1b88066c3e673b9655e4
Rick Bacci <rickbacci@gmail.com>
2015-08-11 16:14:09 -0600

    fix delete user_specialties

--- app/controllers/user_specialties_controller.rb
+++ app/controllers/user_specialties_controller.rb
@@ -1,12 +1,8 @@
 class UserSpecialtiesController < ApplicationController
 
   def destroy
-    #have specialty id need to delete user_specialties that have that id
-    #as specialty_id
-
-    user_specialty = UserSpecialty.find_by(specialty_id: params[:id])
+    user_specialty = UserSpecialty.find_by(specialty_id: params[:specialty_id])
     user_specialty.destroy
-    # current_user.user_specialties.find_by(specialty_id: params[:id]).destroy]
 
     redirect_to current_user
   end
