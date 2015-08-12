4bc95c62203fa2cf26204650129db349232b65ab
Rick Bacci <rickbacci@gmail.com>
2015-08-12 00:15:29 -0600

    add javascript for user specialties

--- app/assets/javascripts/get_specialties.js
+++ app/assets/javascripts/get_specialties.js
@@ -1,24 +1,60 @@
 var availableTags = [];
 
-$(document).ready(function() {
+var keyCodes = {
+  13: 'enter'
+}
 
+$(document).ready(function() {
   getSpecialties();
 
   $("#tags").autocomplete({
     source: availableTags
   });
 
+  // Make an ajax GET request for all specialties for this user
+  $.ajax({
+    url: '/user_specialties',
+    success: function(data) {
+      // Take the raw specialties json, convert to markup,
+      // append to the ul
+      var markup = data.map(renderSpecialtyLi);
+      $('.specialties').append(markup);
+
+      // bindDeleteEvent()
+      $('.delete-specialty').on('click', function() {
+        var id = $(this).data('id');
+
+        $.ajax({
+          url: '/user_specialties/1?specialty_id=' + id, // TODO
+          type: 'DELETE',
+          success: function() {
+            // If the server successfuly destroys the specialty,
+            // remove it from the DOM
+            // removeSpecialty(id)
+            var $specialty = $('.specialties li').filter(function() {
+              return $(this).data('id') === id;
+            });
+            $specialty.remove();
+          }
+        })
+      });
+    },
+    error: function(xhr) {
+      console.log(xhr.responseText)
+    }
+  });
+
   $("#tags").keypress(function(e){
-    if(e.keyCode == 13)
-    {
-      postSpecialties();
-      console.log('key pressed!');
+    if(keyCodes[e.keyCode] === 'enter') {
+      var specialtyName = $('input#tags').val();
+
+      console.log('Pressed enter on specialty form!');
+      postSpecialty(specialtyName);
 
       $('.ui-widget-content').hide();
       $(this).val('');
     }
   });
-
 });
 
 function getSpecialties() {
@@ -27,34 +63,48 @@ function getSpecialties() {
     type: 'GET',
     success: function(tags) {
       $.each(tags, function(index, tag){
-        if($.inArray(tag.specialty_name, availableTags) === -1){
+        if ($.inArray(tag.specialty_name, availableTags) === -1){
           availableTags.push(tag.specialty_name);
-        };
+        }
       });
     }
   });
-
 };
 
-function postSpecialties() {
-  var specialtyName = $('input#tags').val();
+function postSpecialty(specialtyName) {
   $.ajax({
     url: '/specialties',
     type: 'POST',
-    data: {specialty: {specialty_name: specialtyName }},
+    data: {specialty: {specialty_name: specialtyName}},
     dataType: "json",
-    success: function(refreshSpecialties) {
-      console.log("hello world")
-      $('ul.collection').prepend(
-          "<li class='collection-item'>"
-          + refreshSpecialties
-          + "</li>"
-          )
+    success: function(data) {
+      // Append the newly created specialty to the DOM
+      console.log(data);
+      var markup = renderSpecialtyLi(data);
+      $('.specialties').append(markup);
     },
     error: function(xhr) {
       console.log(xhr.responseText)
     }
   });
-
 };
 
+
+/**
+ * Take a specialty as a JS object and return an
+ * html representation.
+ */
+function renderSpecialtyLi(specialty) {
+  return  (
+      "<li data-id='" + specialty.id + "' class='collection-item'>"
+      +   "<div class='row'>"
+      +     "<div class='col s6'>"
+      +       specialty.specialty_name
+      +     "</div>"
+      +     "<div class='col s6 right-align'>"
+      +       "<input data-id='" + specialty.id + "' class='delete-specialty' type='submit' value='Delete'>"
+      +     "</div>"
+      +   "</div>"
+      + "</li>"
+      );
+}
