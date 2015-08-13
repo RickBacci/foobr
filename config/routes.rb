21cd433af5079ad1b71d679b8e7a7ce6a00cf1bf
Rick Bacci <rickbacci@gmail.com>
2015-08-13 10:53:25 -0600

    update production keys

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+
   get '/auth/google_oauth2', as: '/login'
-  get 'auth/:provider/callback', to: 'sessions#create'
+  #get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/google_oauth2callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
